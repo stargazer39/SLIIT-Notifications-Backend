b987c0282e7a9db334f9634dce57882a31c9632e
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-11-08 12:12:37 +0530

    Bulid fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ./dist/react-app
+mkdir -p ./dist/react-app
 
 ( cd dist/react-app ; git clone https://github.com/stargazer39/SLIIT-Notifications-Frontend-js.git ./ )
 ( cd dist/react-app ; npm install && npm run build )
