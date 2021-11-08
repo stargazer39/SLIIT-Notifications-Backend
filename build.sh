6e6804c3e26792f77a0d3906cf74557d610d2731
stargazer39 <dehemiweerakkodi2@gmail.com>
2021-11-08 14:48:33 +0530

    Build script stuff

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+[ -d "./dist/react-app" ] && exit
+
 mkdir -p ./dist/react-app
 
 ( cd dist/react-app ; git clone https://github.com/stargazer39/SLIIT-Notifications-Frontend-js.git ./ )
